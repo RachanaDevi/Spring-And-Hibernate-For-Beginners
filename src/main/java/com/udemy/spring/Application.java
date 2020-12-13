56f5232fbf0d9c46828e9f663395d8ddbfd576ae
Rachana Devi <rachanadevi18@gmail.com>
2020-12-13 22:02:34 +0530

    [Rachana] Add. TrackCoach

--- src/main/java/com/udemy/spring/Application.java
+++ src/main/java/com/udemy/spring/Application.java
@@ -1,14 +1,15 @@
 package com.udemy.spring;
 
-import com.udemy.spring.coach.BaseballCoach;
 import com.udemy.spring.coach.Coach;
+import com.udemy.spring.coach.TrackCoach;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        Coach theCoach = new BaseballCoach();
+        Coach theCoach = new TrackCoach();
         System.out.println(theCoach.getDailyWorkout());
     }
 }
+//Track Coach is still hardcoded, let's try to make it in a config file next
