1b70fc47372f12542bf9550ff729c6cf0e3efcf8
Rachana Devi <rachanadevi18@gmail.com>
2021-01-01 18:56:45 +0530

    [Rachana][Dependency Injection] Adds CricketCoach which uses setter injection for setting private fields emailAddress and team

--- src/main/java/com/udemy/spring/SetterDemoApp.java
+++ src/main/java/com/udemy/spring/SetterDemoApp.java
@@ -1,19 +1,22 @@
 package com.udemy.spring;
 
-import com.udemy.spring.coach.Coach;
+import com.udemy.spring.coach.CricketCoach;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SetterDemoApp {
     public static void main(String[] args) {
+
         // load the spring configuration file
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("file:src/applicationContext.xml");
 
         // retrieve bean from spring container
-        Coach cricketCoach = context.getBean("myCricketCoach", Coach.class);
+        CricketCoach cricketCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         // call methods on the bean
         System.out.println(cricketCoach.getDailyFortune());
+        System.out.println(cricketCoach.getEmailAddress());
+        System.out.println(cricketCoach.getTeam());
 
         //close the context
         context.close();
--- src/main/java/com/udemy/spring/coach/CricketCoach.java
+++ src/main/java/com/udemy/spring/coach/CricketCoach.java
@@ -6,6 +6,10 @@ public class CricketCoach implements Coach {
 
     FortuneService fortuneService;
 
+    // create private fields
+    private String emailAddress;
+    private String team;
+
     //create a no argument constructor
     public CricketCoach() {
         System.out.println("Cricket coach: inside no-arg constructor");
@@ -15,6 +19,24 @@ public class CricketCoach implements Coach {
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("Cricket coach: inside setFortuneService method");
         this.fortuneService = fortuneService;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("Email is getting set as :" + emailAddress);
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("Team is getting set as :" + team);
+        this.team = team;
     }
 
     @Override
