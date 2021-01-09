b4929d203939163153e717015eb3556d9c801026
Rachana Devi <rachanadevi18@gmail.com>
2021-01-09 22:14:58 +0530

    [Rachana][Field injection Annotation] Add. @Autowired to FortuneService in TennisCoach

--- src/main/java/com/udemy/spring/coach/TennisCoach.java
+++ src/main/java/com/udemy/spring/coach/TennisCoach.java
@@ -7,18 +7,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 
+    @Autowired
     private FortuneService fortuneService;
 
     // you don't need to define this, we just put it for debugging purpose
     public TennisCoach() {
         System.out.println("TennisCoach: inside default constructor");
-    }
-
-    //define the setter method having any method name
-    @Autowired
-    public void doSomeCrazyStuff(FortuneService fortuneService) {
-        System.out.println("TennisCoach: inside do some crazy stuff method");
-        this.fortuneService = fortuneService;
     }
 
     @Override
