c5ae6558cf055dbb8c8a0e475bfc27a9b765a6d0
Rachana Devi <rachanadevi18@gmail.com>
2021-01-09 14:42:56 +0530

    [Rachana][Setter method injection Annotation] Add. @Autowired in setter method for fortuneService in TennisCoach

--- src/main/java/com/udemy/spring/coach/TennisCoach.java
+++ src/main/java/com/udemy/spring/coach/TennisCoach.java
@@ -9,8 +9,15 @@ public class TennisCoach implements Coach {
 
     private FortuneService fortuneService;
 
+    // you don't need to define this, we just put it for debugging purpose
+    public TennisCoach() {
+        System.out.println("TennisCoach: inside default constructor");
+    }
+
+    //define the setter method
     @Autowired
-    TennisCoach(FortuneService fortuneService) {
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("TennisCoach: inside setter method");
         this.fortuneService = fortuneService;
     }
 
