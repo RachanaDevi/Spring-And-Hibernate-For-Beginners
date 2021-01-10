031f1e1808682da8923166b2ffe265e72f1a5a72
Rachana Devi <rachanadevi18@gmail.com>
2021-01-10 13:43:53 +0530

    [Rachana][Config with java code] Add. JavaConfigDemoApp and SportConfig

---
+++ src/main/java/com/udemy/spring/JavaConfigDemoApp.java
@@ -0,0 +1,20 @@
+package com.udemy.spring;
+
+import com.udemy.spring.coach.Coach;
+import com.udemy.spring.configuration.SportConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemoApp {
+
+    public static void main(String[] args) {
+        //read spring config java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+
+        // get the bean from spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        // call a method on the bean
+        System.out.println(theCoach.getDailyFortune());
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
---
+++ src/main/java/com/udemy/spring/configuration/SportConfig.java
@@ -0,0 +1,9 @@
+package com.udemy.spring.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.udemy.spring")
+public class SportConfig {
+}
